0dab5da1b1ce84953172d84976ebcd31e5c45f84
giulianofaustini <139208335+giulianofaustini@users.noreply.github.com>
2023-10-21 10:23:40 +0300

    Added form to add workouts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -20,12 +21,12 @@ const Home = () => {
   return (
     <div className="home">
       <div className="workouts">
-              {workouts && workouts.map((workout) => (
-                <WorkoutDetails key={workout._id} workout={workout} />  
-              )
-        
-        )}
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
       </div>
+      <WorkoutForm />
     </div>
   );
 };
