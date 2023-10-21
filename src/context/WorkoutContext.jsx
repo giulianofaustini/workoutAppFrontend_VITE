7d81b6e394e2c56d77732772edabfcf4dd5676e3
giulianofaustini <139208335+giulianofaustini@users.noreply.github.com>
2023-10-21 12:26:00 +0300

    useContext added to wrap the app to update the state.

---
+++ src/context/WorkoutContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useReducer } from "react";
+
+export const WorkoutsContext = createContext();
+
+
+export const WorkoutsReducer = (state, action) => {
+
+
+    switch (action.type) {
+        case 'SET_WORKOUTS':
+            return {
+                workouts: action.payload
+            }
+        case 'CREATE_WORKOUT':
+            return {
+                workouts: [action.payload, ...state.workouts]
+            }
+        default:
+            return state
+    }
+}
+
+export const WorkoutContextProvider = ({ children }) => {
+
+    const [state, dispatch] = useReducer(workoutsReducer, {
+    workouts: null
+    })
+    
+    // dispatch({ type: 'SET_WORKOUTS', payload: [{}, {} ]  })
+
+    return (
+    <WorkoutsContext.Provider value={{state, dispatch}}>
+        {children}
+        </WorkoutsContext.Provider>
+    )
+};
